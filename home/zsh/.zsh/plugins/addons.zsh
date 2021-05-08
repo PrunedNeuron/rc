68f1e7b38222542331d3e8fd2d1cb2085fa76b31
Ayush <aym@pm.me>
2021-05-09 04:25:31 +0530

    changed plugins

--- home/zsh/.zsh/plugins/addons.zsh
+++ home/zsh/.zsh/plugins/addons.zsh
@@ -2,14 +2,13 @@
 
 # Autosuggestions & fast-syntax-highlighting
 zinit wait lucid light-mode for \
-    marlonrichert/zsh-autocomplete \
     atinit"ZINIT[COMPINIT_OPTS]=-C; zpcompinit; zpcdreplay" \
         zdharma/fast-syntax-highlighting \
     atload"!_zsh_autosuggest_start" \
         zsh-users/zsh-autosuggestions \
-        zsh-users/zsh-completions
+        zsh-users/zsh-completions \
+        marlonrichert/zsh-autocomplete
 
-# History and real time autocomplete
 zinit light zsh-users/zsh-history-substring-search
 
 # zsh-autopair (Auto-close and delete matching delimiters)
@@ -17,4 +16,5 @@ zinit light hlissner/zsh-autopair
 
 # Git extras
 zinit lucid wait'0a' for \
-as"program" pick"$ZPFX/bin/git-*" src"etc/git-extras-completion.zsh" make"PREFIX=$ZPFX" tj/git-extras
+    as"program" pick"$ZPFX/bin/git-*" src"etc/git-extras-completion.zsh" make"PREFIX=$ZPFX" \
+        tj/git-extras
