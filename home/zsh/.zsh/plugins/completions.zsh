6fe82128feb855707ce09a025f16fc0023d7fa8d
Ayush <aym@pm.me>
2021-05-09 04:32:27 +0530

    remove unused yarn completions

--- home/zsh/.zsh/plugins/completions.zsh
+++ home/zsh/.zsh/plugins/completions.zsh
@@ -7,10 +7,3 @@ zinit snippet https://github.com/docker/cli/blob/master/contrib/completion/zsh/_
 # Keybase
 zinit ice as"completion"
 zinit snippet https://github.com/rbirnie/oh-my-zsh-keybase/blob/master/keybase/_keybase
-
-## Yarn
-## Disabled atm
-# zinit ice as"completion" \
-#    atload"zpcdreplay" \
-#    atclone'./zplug.zsh'
-# zinit light g-plane/zsh-yarn-autocompletions
