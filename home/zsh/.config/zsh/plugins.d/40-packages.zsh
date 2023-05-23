12d0cab1f2ce506d25e41cb3472eb089302042d9
ayush <aym@protonmail.ch>
2023-05-23 20:02:11 +0200

    2023-05-23_20:02:11

--- home/zsh/.config/zsh/plugins.d/40-packages.zsh
+++ home/zsh/.config/zsh/plugins.d/40-packages.zsh
@@ -2,7 +2,8 @@
 
 # fzf [zinit pack for fzf]
 source "$CONFDIR/fzf/config" && \
-zi has'!fzf' pack for fzf
+zi pack for fzf
+#zi has'!fzf' pack for fzf
 
 # LS_COLORS
-zi pack for ls_colors
+# zi pack for ls_colors
--- home/zsh/.config/zsh/plugins.d/50-programs.zsh
+++ home/zsh/.config/zsh/plugins.d/50-programs.zsh
@@ -5,11 +5,11 @@ zi wait"1" lucid from"gh-r" as"null" for \
         atclone"./zoxide init --cmd j zsh > init.zsh" atpull"%atclone" src"init.zsh" nocompile'!' ajeetdsouza/zoxide \
     sbin'**/bat' \
         mv"**/autocomplete/bat.zsh -> $ZINIT[COMPLETIONS_DIR]/_bat" @sharkdp/bat \
-    sbin'**/exa' \
-        mv"**/exa.zsh -> $ZINIT[COMPLETIONS_DIR]/_exa" sbin"**/exa" ogham/exa \
     sbin'**/vivid' \
         atload'export LS_COLORS="$(vivid generate one-light)"' @sharkdp/vivid \
     sbin'**/delta' \
         atload"alias diff='delta -ns'" dandavison/delta \
     sbin'**/fd' \
         @sharkdp/fd
+#     sbin'exa* -> exa' \
+#         ogham/exa
